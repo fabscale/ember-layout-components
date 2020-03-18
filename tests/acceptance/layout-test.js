7a975a7e6d551e26f0dfb8dd736ae8096c17c947
Francesco Novy <francesco@cropster.com>
2020-03-18 10:30:37 +0100

    doc: Fix dummy app & add very basic test

---
+++ tests/acceptance/layout-test.js
@@ -0,0 +1,13 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | layout', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('dummy app renderes', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+  });
+});
