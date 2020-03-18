5069a4cb2a5f55f732c804c572d91c3c5f652553
Francesco Novy <francesco@cropster.com>
2020-03-18 09:31:44 +0100

    feat: Add withSeparator option to vertical stack

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,12 +1,13 @@
-import Controller from "@ember/controller";
-import { action } from "@ember/object";
-import { tracked } from "@glimmer/tracking";
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
   @tracked clusterSize;
   @tracked clusterPosition;
   @tracked clusterFullWidthOnMobile;
   @tracked verticalStackSize;
+  @tracked verticalStackWithSeparator;
 
   @action
   updateProperty(property, value) {
