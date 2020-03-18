6ace575a11448450c929c2b4f4b6a353cea6649f
Francesco Novy <francesco@cropster.com>
2020-03-18 09:49:53 +0100

    test: Fix tests

--- tests/integration/helpers/layout-class-if-test.js
+++ tests/integration/helpers/layout-class-if-test.js
@@ -8,15 +8,17 @@ module('Integration | Helper | layout-class-if', function(hooks) {
 
   [
     { value: '123', compareValue: '123', result: true },
-    { value: '123', compareValue: '123', result: true },
-    { value: '123', compareValue: '123', result: true },
-    { value: '123', compareValue: '123', result: true }
+    { value: '123', compareValue: 123, result: false },
+    { value: true, compareValue: true, result: true },
+    { value: true, compareValue: false, result: false }
   ].forEach((scenario) => {
     test(`it works with value ${scenario.value} and compare value ${scenario.compareValue}`, async function(assert) {
       this.value = scenario.value;
       this.compareValue = scenario.compareValue;
 
-      await render(hbs`{{layout-class-if inputValue '1234' 'true'}}`);
+      await render(
+        hbs`{{layout-class-if this.value this.compareValue 'true'}}`
+      );
 
       assert.dom(this.element).hasText(scenario.result ? 'true' : '');
     });
