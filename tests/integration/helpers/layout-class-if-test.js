72c3bb274fae8cac72bd421270c143471b2135bb
Francesco Novy <francesco@cropster.com>
2020-03-18 09:45:28 +0100

    chore: Pin volta versions

--- tests/integration/helpers/layout-class-if-test.js
+++ tests/integration/helpers/layout-class-if-test.js
@@ -6,12 +6,19 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Helper | layout-class-if', function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  [
+    { value: '123', compareValue: '123', result: true },
+    { value: '123', compareValue: '123', result: true },
+    { value: '123', compareValue: '123', result: true },
+    { value: '123', compareValue: '123', result: true }
+  ].forEach((scenario) => {
+    test(`it works with value ${scenario.value} and compare value ${scenario.compareValue}`, async function(assert) {
+      this.value = scenario.value;
+      this.compareValue = scenario.compareValue;
 
-    await render(hbs`{{layout-class-if inputValue}}`);
+      await render(hbs`{{layout-class-if inputValue '1234' 'true'}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+      assert.dom(this.element).hasText(scenario.result ? 'true' : '');
+    });
   });
 });
