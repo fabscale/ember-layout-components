de97ee4537bb03a2bba2af825864346ca87abbfa
Francesco Novy <francesco@fabscale.com>
2021-03-02 10:22:15 +0100

    feat: Import from `@ember/template` instead of `@ember/string`
    As the latter has been deprecated in ember 3.25.x

--- addon/helpers/layout-css-var.js
+++ addon/helpers/layout-css-var.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
-import { htmlSafe } from '@ember/string';
+import { htmlSafe } from '@ember/template';
 import { isNone } from '@ember/utils';
 
 export default helper(function layoutCssVar(_, hash) {
